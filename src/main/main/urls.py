bd555717c2b88d3989fc3057a1496fb016b8d2a0
safafofficial@gmail.com <safafofficial@gmail.com>
2023-08-27 15:13:02 +0530

    Database Configuration on Server Completed

--- src/main/main/urls.py
+++ src/main/main/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Include the URL patterns from the contacts app
     path('', include('contacts.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
