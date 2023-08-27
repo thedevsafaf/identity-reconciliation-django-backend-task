7260035c23baab33bd6a5da0b5c693569189c762
safafofficial@gmail.com <safafofficial@gmail.com>
2023-08-27 17:15:49 +0530

    added all allowed hosts in settings

--- main/settings.py
+++ main/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-+nuws-gewzffx7xpzp7_fcebh)fpg&uo2&g@@d13*nh0!ucfv6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
