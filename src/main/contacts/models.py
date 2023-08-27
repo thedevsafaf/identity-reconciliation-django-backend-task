26991c3c0f069bbf7affdb4cb117d33726d0868c
safafofficial@gmail.com <safafofficial@gmail.com>
2023-08-27 13:58:01 +0530

    Contact Model Added

--- src/main/contacts/models.py
+++ src/main/contacts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    phoneNumber = models.CharField(max_length=20, null=True)
+    email = models.EmailField(null=True)
+    linkedId = models.IntegerField(null=True)
+    linkPrecedence = models.CharField(max_length=10)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    deletedAt = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return "Contact " + str(self.id) + " - Email: " + str(self.email)
