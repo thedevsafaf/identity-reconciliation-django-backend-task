f041874eb07abcc453aeab1b88010180f683da46
safafofficial@gmail.com <safafofficial@gmail.com>
2023-08-27 14:37:28 +0530

    All Contacts View Created

--- src/main/contacts/views.py
+++ src/main/contacts/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 import json
 from django.http import JsonResponse
@@ -50,3 +51,11 @@ def identify(request):
             }
 
         return JsonResponse(response_data)   
+    
+# view for all contacts display
+
+def view_contacts(request):
+    contacts = Contact.objects.all()
+    context = {'contacts': contacts}
+    return render(request, 'contacts/view_contacts.html', context)  
+
